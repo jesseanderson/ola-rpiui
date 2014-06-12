c56e027c44e8033fc3087a195ff2a2775ab9702d
Jesse Anderson <jesseranders@gmail.com>
2014-06-12 10:26:28 -0400

    Fixed magic numbers

--- monitorscreen.py
+++ monitorscreen.py
@@ -3,6 +3,10 @@ import kivy
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 
+_CELL_WIDTH = 32
+_CELL_HEIGHT = 32
+_DMX_CHANNELS = 512
+
 class MonitorScreen(Screen):
   """This screen displays the values of as many DMX channels as will fit
      on the screen.
@@ -11,8 +15,9 @@ class MonitorScreen(Screen):
   def __init__(self, **kwargs):
     super(MonitorScreen, self).__init__(**kwargs)
     self.channels = []
-    for channel_index in range(512):
-      channel = Label(text='X',size_hint=(None,None),width=32,height=32)
+    for channel_index in range(_DMX_CHANNELS):
+      channel = Label(text='X',size_hint=(None,None),
+                      width=_CELL_WIDTH,height=_CELL_HEIGHT)
       self.channels.append(channel)
       self.ids.grid.add_widget(channel)
 
@@ -31,4 +36,5 @@ class MonitorScreen(Screen):
     """The grid height must be as high as its last visible child in order
        for the ScrollView to work as intended.
     """
-    self.ids.grid.height = 32 * math.ceil(512.0 / (self.ids.monitor.width / 32))
+    self.ids.grid.height = _CELL_HEIGHT * math.ceil(float(_DMX_CHANNELS) / 
+                             (self.ids.monitor.width / _CELL_WIDTH))
