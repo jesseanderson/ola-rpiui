e2130c0e1106ec55b845b0496e8526cbb69a4890
Jesse Anderson <jesseranders@gmail.com>
2014-07-30 22:42:29 -0700

    Added math for calibration and writing to kivy config

--- calibration.py
+++ calibration.py
@@ -8,6 +8,8 @@ import kivy
 kivy.require('1.8.0')
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.config import Config
+from kivy.uix.layout import Layout
 from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_string('''
@@ -30,20 +32,33 @@ class Calibration(App):
     self.layout.on_touch_up = self.point_1
     return self.layout
 
-  def run_calibration(self):
-    pass
+  def run_calibration(self, p1, p2, p3, p4):
+    avg_x1 = (p1[0] + p3[0]) / 2
+    avg_x2 = (p2[0] + p4[0]) / 2
+    avg_y1 = (p1[1] + p2[1]) / 2
+    avg_y2 = (p3[1] + p3[1]) / 2
+    min_position_x = avg_x1 - (avg_x2 - avg_x1) / 2
+    max_position_x = avg_x2 + (avg_x2 - avg_x1) / 2
+    min_position_y = avg_y1 - (avg_y2 - avg_y1) / 2
+    max_position_y = avg_y2 + (avg_y2 - avg_y1) / 2
+    Config.set('input', 'ts', 'hidinput,/dev/input/touchscreen'
+                              ',min_position_x=' + str(int(min_position_x)) +
+                              ',max_position_x=' + str(int(max_position_x)) +
+                              ',min_position_y=' + str(int(min_position_y)) +
+                              ',max_position_y=' + str(int(max_position_y)))
+    Config.write()
 
   def point_1(self, event):
-    print event
+    self.layout.on_touch_up = lambda e: self.point_2(e, event.pos)
 
-  def point_2(self, event):
-    pass
+  def point_2(self, event, p1):
+    self.layout.on_touch_up = lambda e: self.point_3(e, p1, event.pos)
 
-  def point_3(self, event):
-    pass
+  def point_3(self, event, p1, p2):
+    self.layout.on_touch_up = lambda e: self.point_4(e, p1, p2, event.pos)
 
-  def point_4(self, event):
-    pass
+  def point_4(self, event, p1, p2, p3):
+    self.run_calibration(p1, p2, p3, event.pos)
 
 if __name__ == '__main__':
   Calibration().run()
